3126681f8ae2ef82ce08405ce8f9cadab5eac088
Zachary Blackwood <zachary@streamlit.io>
2023-02-21 15:30:52 -0500

    Add manifest

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-vizzu",
-    version="0.1.0",
+    version="0.1.1",
     author="Zachary Blackwoood",
     author_email="zachary@streamlit.io",
     description=(
